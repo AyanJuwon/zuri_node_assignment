012a24b08d1c37fa545b4b4abef6fc50b7465dcc
Ayanniran Oluwajuwon <oiayanniran@student.lautech.edu.ng>
2021-04-16 10:49:14 +0100

    completed task

--- Task_2/script.js
+++ Task_2/script.js
@@ -1,30 +1,34 @@
-
+// Require the http module to creat node server
 const http = require("http");
 
-
-
-const fs = require('fs');
+// require the fs module to manipulate files
+const fs = require("fs");
+// function create node server
 const server = http.createServer(function (req, res) {
-    res.writeHead(200, { "content-type": "text/html" });
-    get_data();
-    res.end( 'Data transferred');
-    
-    
+  res.writeHead(200, { "content-type": "text/html" });
+  // call the json filr function
+  get_data();
+  // display a form of success image on the page
+  res.end("Data transferred");
 });
-
+// call the server
 server.listen(4000, "localhost");
 
 console.log("server created");
 
-function get_data(){
-    const fetch = require('node-fetch');
-    const url  = 'http://jsonplaceholder.typicode.com/posts'
-    fetch(url)
-        .then((resp) => resp.json())
-        .then( json =>{
-            data = JSON.stringify(json,null,2)
-            fs.writeFileSync('./result/posts.json', data)
-            fs.writeFileSync('./result/posts.txt', data)
-        }
-    )
+// DUnction to get data from API
+function get_data() {
+  // require fetch API
+  const fetch = require("node-fetch");
+  const url = "http://jsonplaceholder.typicode.com/posts";
+  fetch(url)
+    .then((resp) => resp.json())
+    // Get data and save to local variable
+    .then((json) => {
+      //    stringify your data and make it look neat
+      data = JSON.stringify(json, null, 2);
+      // Write files to result/posts
+      fs.writeFileSync("./result/posts.json", data);
+      fs.writeFileSync("./result/posts.txt", data);
+    });
 }
